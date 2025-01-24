56ed2e2e94da00ec667bd1ac2ec8f7835e4049ee
dietherocampo19 <dietherjohn.ocampo@dcmail.ca>
2025-01-24 14:00:52 -0500

    classlist

--- ICE/src/client/app/animals/index.js
+++ ICE/src/client/app/animals/index.js
@@ -14,6 +14,39 @@ function animal() {
         const container = document.createElement('div');
         container.classList.add('mb-2');
         //create animal form content
+        const mb3Name = document.createElement('div');
+        mb3Name.classList.add('mb-3');
+        mb3Name.innerHTML = '<label for="name" class="form-label">Animal Name</label>'
+                '<input type="text" class="form-control" id="name" name="name">'
+                '<p class="text-danger d-none">currently hidden</p>'
+        container.append(mb3Name);
+        const mb3Breed = document.createElement('div');
+        mb3Breed.classList.add('mb-3');
+        mb3Brred.innerHTML = '<label for="breed" class="form-label">Animal Breed</label>'
+        '<input type="text" class="form-control" id="breed" name="breed">'
+        '<p class="text-danger d-none"></p>'
+        container.append(mb3Breed);
+        const mb3Legs = document.createElement('div');
+        mb3Legs.classList.add('mb-3');
+        mb3Legs.innerHTML = '<label for="legs" class="form-label">Number of Legs</label>'
+        '<input type="text" class="form-control" id="legs" name="legs">'
+        '<p class="text-danger d-none"></p>'
+        container.append(mb3Legs);
+        const mb3Eyes = document.createElement('div');
+        mb3Eyes.classList.add('mb-3');
+        mb3Eyes.innerHTML = '<label for="eyes" class="form-label">Number of Eyes</label>'
+        '<input type="text" class="form-control" id="eyes" name="eyes">'
+        '<p class="text-danger d-none"></p>'
+        container.append(mb3Eyes);
+        const mb3Sound = document.createElement('div');
+        mb3Sound.classList.add('mb-3');
+        mb3Sound.innerHTML ='<label for="sound" class="form-label">Sound this animal makes</label>'
+                '<input type="text" class="form-control" id="sound" name="sound">'
+                '<p class="text-danger d-none"></p>'
+        container.append(mb3Sound;)
+        
+
+
 
         ///
         form.append(container);
