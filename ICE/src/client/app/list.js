4283c1d746f460d0af57f7292bdbfff862305567
dietherocampo19 <dietherjohn.ocampo@dcmail.ca>
2025-03-14 01:08:32 -0400

    Update list.js

--- ICE/src/client/app/list.js
+++ ICE/src/client/app/list.js
@@ -7,7 +7,7 @@
 */
 
 
-import animalService from "./animal.mock.service.js";
+import animalService from "./animal.service.js";
 
 function list(recordPage) {
     const container = document.createElement('div');
